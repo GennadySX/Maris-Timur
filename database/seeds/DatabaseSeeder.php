8f7186897aaf4a11a9f6eed199ef66c6f955b7dc
GennadySX <htcucell@gmail.com>
2020-04-06 03:35:09 +0300

    m -05 :stc 1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $this->call(UserSeed::class);
+        $this->call(CustomerSeed::class);
+        $this->call(RepairSeed::class);
+        $this->call(CallSeed::class);
+        $this->call(ReceiptSeed::class);
     }
 }
---
+++ database/seeds/ReceiptSeed.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ReceiptSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
